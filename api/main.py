4b52089b729f7dd18f3a27bbf6f9a0518de73260
sameersayani <sameersayani@gmail.com>
2025-03-17 02:42:15 +0530

    added manum for deployment

--- api/main.py
+++ api/main.py
@@ -1,10 +1,14 @@
+import os
 import uvicorn
-from app import app 
+from app import app  # Importing FastAPI instance from app module
+from mangum import Mangum  # Required for Passenger (ASGI compatibility)
 
 if __name__ == "__main__":
     uvicorn.run(
-        app="api.main:app",
-        host="localhost",
-        port=8000,
-        reload=True
+        app,
+        host="0.0.0.0",  # Required for cPanel
+        port=int(os.environ.get("PORT", 8000)),  # Fetch port dynamically
+        reload=False  # Disable auto-reload in production
     )
+else:
+    application = Mangum(app)  # Required for cPanel's Passenger
