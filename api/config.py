d96a0aa9f810686c8e0ca138d850e99faf9a55c7
sameersayani <sameersayani@gmail.com>
2025-03-17 05:29:46 +0530

    updated config file

--- api/config.py
+++ api/config.py
@@ -9,4 +9,4 @@ CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
 GOOGLE_REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI', None)
 API_BASE_URL = os.environ.get('API_BASE_URL', None)
 REACT_BASE_URL = os.environ.get('REACT_BASE_URL', None)
-DATABASE_URL = "postgres://postgres:sameer@localhost:5432/smartexpensedb"
+DATABASE_URL = os.environ.get('DATABASE_URL', None)
